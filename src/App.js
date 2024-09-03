1960b2f37439382c6e99f3e1d18294101651a1b8
Charchit <charchitpaharia@gmail.com>
2024-09-04 00:41:42 +0530

    1 - Created Basic UI with help of tailwind with home page.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import  ProductList  from './features/product-list/ProductList';
+import Home from './pages/home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Counter></Counter> */}
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
