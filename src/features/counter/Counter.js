2031b86d9aae5ed0051d2dcbf7c946611255ae81
Charchit <charchitpaharia@gmail.com>
2024-09-04 13:12:57 +0530

    3 - Add Cart Page and Feature

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  increment,
-  incrementAsync,
-} from './counterSlice';
 
 export default function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
 
   const incrementValue = Number(incrementAmount) || 0;
 
