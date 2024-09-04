47c2354ef088e2eb9e9c8b791c0a151620eb33a0
Charchit <charchitpaharia@gmail.com>
2024-09-04 16:40:40 +0530

    4 - Added 2 more pages Product Detail and CheckOut page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import  ProductList  from './features/product-list/ProductList';
+import  ProductList  from './features/product-list/component/ProductList';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import ProductDetail from  './pages/ProductDetailPage'
 import './App.css';
 
 
@@ -30,6 +32,14 @@ const router = createBrowserRouter([
     path: "/cart",
     element: <CartPage></CartPage>,
   },
+  {
+    path: "/checkout",
+    element: <Checkout></Checkout>,
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetail></ProductDetail>
+  }
 ]);
 
 
