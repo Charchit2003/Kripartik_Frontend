802edb248599d4c26c4543d7c1a44587f33fdb46
Charchit <charchitpaharia@gmail.com>
2024-09-04 12:32:57 +0530

    2 - Create login and signup as features and pages and created Dynamic Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import  ProductList  from './features/product-list/ProductList';
-import Home from './pages/home';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 import './App.css';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter></Counter> */}
-      <Home></Home>
+      {/* <SignupPage></SignupPage> */}
+
+      {/* Dynamic Routing */}
+      <RouterProvider router={router} />
     </div>
   );
 }
